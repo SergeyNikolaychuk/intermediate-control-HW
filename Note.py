594a467212073e9800cbcc4518ba3202fc8889ca
Sergey_Nikolaychuk <nikolaychuk29@mail.ru>
2023-07-20 21:42:48 +0400

    delete_cod

--- Note.py
+++ Note.py
@@ -9,14 +9,3 @@ class Notebook:
 
     def str(self):
         return f"Notebook(brand='{self.brand}', model='{self.model}', ram={self.ram}, storage={self.storage}, os='{self.os}', color='{self.color}')"
-
-class NotebookShop:
-    def init(self, notebooks):
-        self.notebooks = notebooks
-
-    def filter_notebooks(self):
-        min_ram = int(input("Enter the minimum RAM size (in GB): "))
-        color = input("Enter the color: ")
-        min_storage = int(input("Enter the minimum storage size (in GB): "))
-
-        return [notebook for notebook in self.notebooks if notebook.ram >= min_ram and notebook.color.lower() == color.lower() and notebook.storage >= min_storage]
